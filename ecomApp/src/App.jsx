450cde36ba52ac30c67a8fb94c1f1530c749882b
unknown <teotiapriyanshu1@gmail.com>
2026-02-19 14:08:51 +0530

    message

---
+++ ecomApp/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import Ecom from './Ecom'
+
+export default function App() {
+  return (
+    <>
+      <Ecom/>
+    </>
+  )
+}
---
+++ ecomApp/src/Ecom.jsx
@@ -0,0 +1,7 @@
+export default function Ecom() {
+    return (
+        <div>
+            <h1>Welcome to Ecom App</h1>
+        </div>
+    )
+}
